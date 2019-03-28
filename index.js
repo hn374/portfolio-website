6fbeb6a573dd299dcd61108b15d328a342069d6a
Hoang Nguyen <hn374@drexel.edu>
2019-03-27 22:08:10 -0400

    Made it able to show the css

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
-app.get('/', function(req, res) {
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/assets')));
+
+/* app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, "/index.html"));
+}); */
+
+app.get('/', function(req, res) {
+	res.render('index');
 });
 
 const PORT = process.env.PORT || 8000;
